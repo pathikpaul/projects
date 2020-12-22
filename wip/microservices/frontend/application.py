56a0b84a9134939f07b00bd0ab6d393a6803c13d
Pathik Paul <you@example.com>
2020-12-22 03:46:48 +0000

    changes

--- wip/microservices/frontend/application.py
+++ wip/microservices/frontend/application.py
@@ -7,20 +7,58 @@ import json
 #import os.path
 #import sys
 import requests
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 
 application = Flask(__name__)
 
+payload = { "username":"admin", "password":"12345" }
+jwtToken = ""
+ErrorMessage=""
+
+@application.route('/moviedetails/<string:movieid>',methods=["GET"])
+def get_moviedetails(movieid):
+    global ErrorMessage
+    r = requests.get("http://192.168.77.10:5001/movies/"+movieid, headers={'Authorization': 'Bearer '+jwtToken})
+    if r.status_code != 200:
+       print(type(r.text),r.text)
+       print(r.text)
+       ErrorMessage=r.text
+       ErrorMessage="Hello world"
+       return redirect(url_for('welcome'))
+    print(r)
+    jj = json.loads(r.text)
+    return render_template("moviedetails.html",notes=jj)
+
 @application.route('/')
 def welcome():
+    global ErrorMessage
+    NewErrorMessage=ErrorMessage
+    ErrorMessage=""
+    ## Invloke Login URL to Get the token
+    #r = requests.post("http://192.168.77.10:5001/movies/login",json=payload)
+    #print(r)
+    #jj = json.loads(r.text)
+    #jwtToken = jj["jwt"]
+    #print ("jwtToken=", jwtToken)
+    ##
     r = requests.get("http://192.168.77.10:5002/showtimes")
     jj = json.loads(r.text)
-    return render_template("welcome.html",notes=jj)
+    return render_template("welcome.html",notes=jj,message=NewErrorMessage)
 if __name__ == "__main__":
-    #r = requests.get("http://192.168.77.10:5002/showtimes")
-    #jj = json.loads(r.text)
-    #for key in jj.keys():
-    #    print(key)
-    #    for movie in jj[key]:
-    #        print(movie)
+    #
+    ## Invloke Login URL to Get the token
+    r = requests.post("http://192.168.77.10:5001/movies/login",json=payload)
+    print(r)
+    jj = json.loads(r.text)
+    jwtToken = jj["jwt"]
+    print ("jwtToken=", jwtToken)
+    #
+    r = requests.get("http://192.168.77.10:5001/movies/720d006c-3a57-4b6a-b18f-9b713b073f3c", headers={'Authorization': 'Bearer '+jwtToken})
+    if r.status_code != 200:
+       print(r.text)
+       exit (1)
+    print(r)
+    jj = json.loads(r.text)
+    for key in jj.keys():
+        print(key,jj[key])
     application.run(port=5009, host= '0.0.0.0' ,debug=True)
--- wip/microservices/services/movies.py
+++ wip/microservices/services/movies.py
@@ -1,9 +1,13 @@
 from common import root_dir, nice_json
 from flask import Flask
 from flask_cors import CORS
-from werkzeug.exceptions import NotFound
+#from werkzeug.exceptions import NotFound
 import json
-
+from flask import request
+import jwt
+import datetime
+from flask import abort
+SECRET_KEY = 'my_precious'
 
 app = Flask(__name__)
 CORS(app)
@@ -22,8 +26,8 @@ def hello():
         }
     })
 
-@app.route("/movies/<movieid>", methods=['GET'])
-def movie_info(movieid):
+@app.route("/movies/public/<movieid>", methods=['GET'])
+def movie_pbulic_info(movieid):
     if movieid not in movies:
         raise NotFound
 
@@ -32,10 +36,66 @@ def movie_info(movieid):
 
     return nice_json(result)
 
+@app.route("/movies/<movieid>", methods=['GET'])
+def movie_info(movieid):
+    myHeader=request.headers.get('Authorization')
+    if myHeader is None:
+        abort(400, "Missign Authorization Token")
+    if not myHeader.startswith('Bearer '):
+        abort(400, "Missign Bearer Token")
+    bearer,_,auth_token = myHeader.partition(' ')
+    try:
+        payload=decode_auth_token(auth_token)
+    except jwt.ExpiredSignatureError:
+        abort(400, "Signature expired. Please log in again.")
+    except jwt.InvalidTokenError:
+        abort(400, "Invalid token. Please log in again.")
+    print('payload=',type(payload),payload)
+    if movieid not in movies:
+        abort(400,"Movie not found in List of Movies")
+
+    result = movies[movieid]
+    result["uri"] = "/movies/{}".format(movieid)
+
+    return nice_json(result)
+
+@app.route("/movies/login", methods=['POST'])
+def movie_login():
+    myinput = request.json
+    if myinput is None:
+        abort(400, "username and password needs to be provided in json format" )
+    if 'username' not in myinput.keys():
+        abort(400, "username needs to be provided " )
+    if 'password' not in myinput.keys():
+        abort(400, "username needs to be provided " )
+    if myinput["username"] != "admin" or myinput["password"] != "12345":
+        abort(400, "Invalid username or password provided." )
+    
+    return nice_json({'username':myinput["username"],'jwt':encode_auth_token(myinput["username"])})
+
 
 @app.route("/movies", methods=['GET'])
 def movie_record():
     return nice_json(movies)
+
+def decode_auth_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, SECRET_KEY)
+        return payload
+    except:
+        print ('invalid token')
+        raise
+
+def encode_auth_token(user_id):
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=10),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    except Exception as e:
+        return e
 
 
 if __name__ == "__main__":
